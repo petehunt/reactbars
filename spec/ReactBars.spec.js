e70064df19192d25b60feba6b1bd84203b7940aa
petehunt <floydophone@gmail.com>
2014-01-07 19:58:05 -0800

    it makes something somewhat like jsx

--- spec/ReactBars.spec.js
+++ spec/ReactBars.spec.js
@@ -1,6 +1,8 @@
 var jsdom = require('jsdom');
 var ReactBars = require('../lib/ReactBars');
 
+var HBS_EXAMPLE = '<ul class="wat">{{#posts}}<li>{{{link_to}}}</li>{{/posts}}</ul>';
+
 describe('ReactBars', function() {
   it('can convert hbs to jsx without dying completely', function() {
     var done = false;
@@ -11,7 +13,7 @@ describe('ReactBars', function() {
         scripts: [],
         done: function (err, window) {
           expect(!!err).toBe(false);
-          expect(ReactBars.convertToJSX(window.document, '<ul class="wat">{{#posts}}<li>{{{link_to}}}</li>{{/posts}}</ul>')).toBe(
+          expect(ReactBars.convertToJSX(window.document, HBS_EXAMPLE)).toBe(
             '<ul className="wat">{{#posts}}<li>{{{link_to}}}</li>{{/posts}}</ul>\n'
           );
 
@@ -34,7 +36,33 @@ describe('ReactBars', function() {
         scripts: [],
         done: function (err, window) {
           expect(!!err).toBe(false);
-          expect(ReactBars.parseHandlebars(window.document, '<ul class="wat">{{#posts}}<li>{{{link_to}}}</li>{{/posts}}</ul>').statements.length).toBe(3);
+          expect(ReactBars.parseHandlebars(window.document, HBS_EXAMPLE).statements.length).toBe(3);
+          done = true;
+        }
+      });
+    });
+
+    waitsFor(function() {
+      return done;
+    });
+  });
+
+  it('can do its job', function() {
+    var done = false;
+
+    runs(function() {
+      jsdom.env({
+        html: "<html><body></body></html>",
+        scripts: [],
+        done: function (err, window) {
+          expect(!!err).toBe(false);
+          try {
+            expect(ReactBars.rewriteHandlebars(window.document, HBS_EXAMPLE)).toBe(
+              '<ul className="wat"><posts><li>{this.props.link_to}</li></posts></ul>\n'
+            );
+          } catch (e) {
+            console.error(e);
+          }
           done = true;
         }
       });
