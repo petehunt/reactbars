213491998e74610060a957545c3bcfa53fcda03a
petehunt <floydophone@gmail.com>
2014-01-07 20:23:46 -0800

    if

--- spec/ReactBars.spec.js
+++ spec/ReactBars.spec.js
@@ -72,4 +72,30 @@ describe('ReactBars', function() {
       return done;
     });
   });
+
+  it('can handle ifs', function() {
+    var done = false;
+
+    runs(function() {
+      jsdom.env({
+        html: "<html><body></body></html>",
+        scripts: [],
+        done: function (err, window) {
+          expect(!!err).toBe(false);
+          try {
+            expect(ReactBars.rewriteHandlebars(window.document, '<h1>{{#if abcd}}hello{{else}}goodbye{{/if}} sup</h1>')).toBe(
+              '<h1>{ ((this.props.abcd) ? <span>hello</span> : <span>goodbye</span>) } sup</h1>\n'
+            );
+          } catch (e) {
+            console.error(e);
+          }
+          done = true;
+        }
+      });
+    });
+
+    waitsFor(function() {
+      return done;
+    });
+  });
 });
