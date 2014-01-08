4516a618b1cd32803c62915adf50250abcbbf71b
petehunt <floydophone@gmail.com>
2014-01-07 21:57:16 -0800

    done

--- spec/ReactBars.spec.js
+++ spec/ReactBars.spec.js
@@ -1,5 +1,6 @@
 var jsdom = require('jsdom');
 var ReactBars = require('../lib/ReactBars');
+var React = require('react-tools').React;
 var transform = require('react-tools').transform;
 
 var HBS_EXAMPLE = '<ul class="wat">{{#yolo posts}}<li>{{{link_to}}}</li>{{/yolo}}</ul>';
@@ -59,7 +60,7 @@ describe('ReactBars', function() {
           expect(!!err).toBe(false);
           try {
             expect(ReactBars.rewriteHandlebars(window.document, HBS_EXAMPLE)).toBe(
-              '<ul className="wat"><yolo params={[posts]}><li>{link_to}</li></yolo></ul>\n'
+              '<ul className="wat">{ (yolo).map(function(item) { with (__this = item) { return (<span><li>{link_to}</li></span>); } }) }</ul>'
             );
           } catch (e) {
             console.error(e);
@@ -115,6 +116,79 @@ describe('ReactBars', function() {
             console.error(e);
           }
           done = true;
+        }
+      });
+    });
+
+    waitsFor(function() {
+      return done;
+    });
+  });
+
+  it('should work gosh darn it!', function() {
+    var done = false;
+
+    // stolen from http://jsfiddle.net/gazraa/xfWKF/
+
+    var SRC = '<table>\n\
+    <thead> \n\
+        <th>Name</th> \n\
+        <th>Job Title</th> \n\
+        <th>Twitter</th> \n\
+    </thead> \n\
+    <tbody> \n\
+        {{#users}} \n\
+        <tr> \n\
+            <td>{{person.firstName}} {{person.lastName}}</td> \n\
+            <td>{{jobTitle}}</td> \n\
+            <td><a href="https://twitter.com/{{twitter}}">@{{twitter}}</a></td> \n\
+        </tr> \n\
+        {{/users}} \n\
+    </tbody> \n\
+</table> \n\
+';
+
+    var DATA = {
+      users: [ {
+        person: {
+          firstName: "Garry",
+          lastName: "Finch"
+        },
+        jobTitle: "Front End Technical Lead",
+        twitter: "gazraa"
+      }, {
+        person: {
+          firstName: "Garry",
+          lastName: "Finch"
+        },
+        jobTitle: "Photographer",
+        twitter: "photobasics"
+      }, {
+        person: {
+          firstName: "Garry",
+          lastName: "Finch"
+        },
+        jobTitle: "LEGO Geek",
+        twitter: "minifigures"
+      } ]
+    };
+
+    runs(function() {
+      jsdom.env({
+        html: "<html><body></body></html>",
+        scripts: [],
+        done: function (err, window) {
+          expect(!!err).toBe(false);
+          try {
+            var template = ReactBars.compile(SRC, 'Template', React, window.document);
+            React.renderComponentToString(template(DATA), function(markup) {
+              // manually verified in jsfiddle to be correct, also not stable
+              expect(markup.length > 0).toBe(true);
+              done = true;
+            });
+          } catch (e) {
+            console.error(e);
+          }
         }
       });
     });
